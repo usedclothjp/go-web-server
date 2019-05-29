db4cef98bf1f6d01b1be0ac92db110ee377a5876
usedclothjp <usedclothjp@gmail.com>
2019-05-29 10:04:01 +0900

    add permission to main.go

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/work/go-web-server
+
+go 1.12
--- main.go
+++ main.go
@@ -9,6 +9,6 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello")
 	})
-	
+
 	http.ListenAndServe(":8080", nil)
 }
